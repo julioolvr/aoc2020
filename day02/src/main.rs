c8d75345190b452c196371b20b18256b6cb71351
Julio <julio.olvr@gmail.com>
2020-12-02 09:31:16 -0300

    Day 2 Part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -10,24 +10,40 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(file);
 
     let regex = Regex::new(r"(\d+)-(\d+) ([a-z]): (.+)").expect("Failed to compile regex");
-    let passwords = reader.lines().map(|line| {
-        let line = line.expect("Could not read line");
-        let captures = regex.captures(&line).expect("Line did not match regex");
+    let passwords: Vec<Password> = reader
+        .lines()
+        .map(|line| {
+            let line = line.expect("Could not read line");
+            let captures = regex.captures(&line).expect("Line did not match regex");
 
-        let lower_bound: usize = captures[1].parse().expect("Unable to parse lower bound");
-        let upper_bound: usize = captures[2].parse().expect("Unable to parse upper bound");
-        let letter: char = captures[3]
-            .chars()
-            .nth(0)
-            .expect("Could not extract policy letter");
-        let password = String::from(&captures[4]);
+            let first_index: usize = captures[1].parse().expect("Unable to parse lower bound");
+            let second_index: usize = captures[2].parse().expect("Unable to parse upper bound");
+            let letter: char = captures[3]
+                .chars()
+                .nth(0)
+                .expect("Could not extract policy letter");
+            let password = String::from(&captures[4]);
 
-        Password::new(password, lower_bound, upper_bound, letter)
-    });
+            Password::new(password, first_index, second_index, letter)
+        })
+        .collect();
 
     println!(
         "Part 1: {}",
-        passwords.filter(|password| password.is_valid()).count()
+        passwords
+            .iter()
+            .by_ref()
+            .filter(|password| password.is_valid())
+            .count()
+    );
+
+    println!(
+        "Part 2: {}",
+        passwords
+            .iter()
+            .by_ref()
+            .filter(|password| password.is_really_valid())
+            .count()
     );
 
     Ok(())
@@ -35,8 +51,8 @@ fn main() -> io::Result<()> {
 
 struct Policy {
     letter: char,
-    lower_bound: usize,
-    upper_bound: usize,
+    first_index: usize,
+    second_index: usize,
 }
 
 struct Password {
@@ -45,12 +61,12 @@ struct Password {
 }
 
 impl Password {
-    fn new(password: String, lower_bound: usize, upper_bound: usize, letter: char) -> Password {
+    fn new(password: String, first_index: usize, second_index: usize, letter: char) -> Password {
         Password {
             password,
             policy: Policy {
-                lower_bound,
-                upper_bound,
+                first_index,
+                second_index,
                 letter,
             },
         }
@@ -58,6 +74,19 @@ impl Password {
 
     fn is_valid(&self) -> bool {
         let occurrences = self.password.matches(self.policy.letter).count();
-        (self.policy.lower_bound..=self.policy.upper_bound).contains(&occurrences)
+        (self.policy.first_index..=self.policy.second_index).contains(&occurrences)
+    }
+
+    // For the actual policies described in part 2
+    fn is_really_valid(&self) -> bool {
+        let mut chars = self.password.chars();
+        let first_char = chars
+            .nth(self.policy.first_index - 1)
+            .expect("Out of bounds char");
+        let second_char = chars
+            .nth(self.policy.second_index - self.policy.first_index - 1)
+            .expect("Out of bounds char");
+
+        (first_char == self.policy.letter) ^ (second_char == self.policy.letter)
     }
 }
