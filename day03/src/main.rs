d47fda8170bb5924f7be51f68acc59d99467052b
Julio <julio.olvr@gmail.com>
2020-12-03 10:02:33 -0300

    Day 3 Part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -6,22 +6,42 @@ use std::{
 fn main() -> io::Result<()> {
     let file = File::open("./input.txt")?;
     let reader = BufReader::new(file);
-
-    let result = reader
+    let lines: Vec<String> = reader
         .lines()
         .map(|line| line.expect("Couldn't read line"))
+        .collect();
+
+    let result = count_trees(&lines, 3);
+    println!("Part 1: {}", result);
+
+    let right_1 = count_trees(&lines, 1);
+    let right_5 = count_trees(&lines, 5);
+    let right_7 = count_trees(&lines, 7);
+
+    // Filter to skip every other line since in the next slope we go two lines down each step
+    let lines: Vec<String> = lines
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, line)| if i % 2 == 0 { Some(line) } else { None })
+        .collect();
+
+    let down_2 = count_trees(&lines, 1);
+
+    println!("Part 2: {}", result * right_1 * right_5 * right_7 * down_2);
+    Ok(())
+}
+
+fn count_trees(lines: &Vec<String>, shift_right: usize) -> usize {
+    lines
+        .iter()
         .skip(1) // We won't count trees in the first line
         .enumerate()
         .filter(|(i, line)| {
             let line_length = line.len();
             line.chars()
-                .nth((3 * (i + 1)) % line_length)
+                .nth((shift_right * (i + 1)) % line_length)
                 .expect("Character out of bounds")
                 == '#'
         })
-        .count();
-
-    println!("Part 1: {}", result);
-
-    Ok(())
+        .count()
 }
