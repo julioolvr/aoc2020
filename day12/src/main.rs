8c15c9945c109ae6dd0f2b49ba2838e2c958410a
Julio <julio.olvr@gmail.com>
2020-12-12 12:43:37 -0300

    Day 12 Part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -9,39 +9,107 @@ fn main() {
     let file = File::open("./input.txt").expect("Unable to open file");
     let reader = BufReader::new(file);
 
+    let parse_regex = Regex::new(r"(\w)(\d+)").unwrap();
+
+    let instructions: Vec<Instruction> = reader
+        .lines()
+        .map(|line| line.unwrap())
+        .map(|line| {
+            let captures = parse_regex.captures(&line).unwrap();
+            let action = &captures[1];
+            let amount: isize = captures[2].parse().unwrap();
+
+            match action {
+                "N" => Instruction::North(amount),
+                "S" => Instruction::South(amount),
+                "E" => Instruction::East(amount),
+                "W" => Instruction::West(amount),
+                "L" => Instruction::Left(amount),
+                "R" => Instruction::Right(amount),
+                "F" => Instruction::Forward(amount),
+                _ => unreachable!(),
+            }
+        })
+        .collect();
+
     let mut vertical: isize = 0;
     let mut horizontal: isize = 0;
     let mut orientation = Orientation::east();
 
-    let parse_regex = Regex::new(r"(\w)(\d+)").unwrap();
-
-    reader.lines().map(|line| line.unwrap()).for_each(|line| {
-        let captures = parse_regex.captures(&line).unwrap();
-        let action = &captures[1];
-        let amount: isize = captures[2].parse().unwrap();
-
-        match action {
-            "N" => vertical += amount,
-            "S" => vertical -= amount,
-            "E" => horizontal += amount,
-            "W" => horizontal -= amount,
-            "L" => orientation.rotate(-amount),
-            "R" => orientation.rotate(amount),
-            "F" => match orientation.direction() {
+    instructions
+        .iter()
+        .for_each(|instruction| match instruction {
+            Instruction::North(amount) => vertical += amount,
+            Instruction::South(amount) => vertical -= amount,
+            Instruction::East(amount) => horizontal += amount,
+            Instruction::West(amount) => horizontal -= amount,
+            Instruction::Left(amount) => orientation.rotate(-amount),
+            Instruction::Right(amount) => orientation.rotate(*amount),
+            Instruction::Forward(amount) => match orientation.direction() {
                 Direction::North => vertical += amount,
                 Direction::South => vertical -= amount,
                 Direction::East => horizontal += amount,
                 Direction::West => horizontal -= amount,
             },
-            _ => unreachable!(),
-        }
-    });
+        });
 
     let part_1 = vertical.abs() + horizontal.abs();
     println!("Part 1: {}", part_1);
+
+    let mut vertical: isize = 0;
+    let mut horizontal: isize = 0;
+    let mut waypoint_x: isize = 10;
+    let mut waypoint_y: isize = 1;
+
+    instructions
+        .iter()
+        .for_each(|instruction| match instruction {
+            Instruction::North(amount) => waypoint_y += amount,
+            Instruction::South(amount) => waypoint_y -= amount,
+            Instruction::East(amount) => waypoint_x += amount,
+            Instruction::West(amount) => waypoint_x -= amount,
+            Instruction::Left(amount) => {
+                let (x, y) = rotate(waypoint_x, waypoint_y, -amount);
+                waypoint_x = x;
+                waypoint_y = y;
+            }
+            Instruction::Right(amount) => {
+                let (x, y) = rotate(waypoint_x, waypoint_y, *amount);
+                waypoint_x = x;
+                waypoint_y = y;
+            }
+            Instruction::Forward(amount) => {
+                horizontal += waypoint_x * amount;
+                vertical += waypoint_y * amount;
+            }
+        });
+
+    let part_2 = vertical.abs() + horizontal.abs();
+    println!("Part 2: {}", part_2);
 }
 
-#[derive(Debug)]
+fn rotate(x: isize, y: isize, degrees: isize) -> (isize, isize) {
+    let rotation = ((degrees / 90) + 4) % 4;
+
+    match rotation {
+        0 => (x, y),
+        1 => (y, -x),
+        2 => (-x, -y),
+        3 => (-y, x),
+        _ => unreachable!(),
+    }
+}
+
+enum Instruction {
+    North(isize),
+    South(isize),
+    East(isize),
+    West(isize),
+    Left(isize),
+    Right(isize),
+    Forward(isize),
+}
+
 enum Direction {
     North,
     South,
