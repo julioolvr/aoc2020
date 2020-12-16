63eeeade27467ec4e961efd8888cedabd1c3a43d
Julio <julio.olvr@gmail.com>
2020-12-16 00:43:56 -0300

    Day 15 Part 2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,26 +1,27 @@
 use std::collections::HashMap;
 
-static SAMPLE: &[usize] = &[0, 3, 6];
 static INPUT: &[usize] = &[2, 15, 0, 9, 1, 20];
 
-const GOAL: usize = 2020;
-
 fn main() {
+    println!("Part 1: {}", solve(2020));
+    println!("Part 2: {}", solve(30_000_000));
+}
+
+fn solve(goal: usize) -> usize {
     let mut last_index: HashMap<usize, usize> = HashMap::new();
 
-    let mut last_number = 0;
+    let (last_number, predefined_numbers) = INPUT.split_last().unwrap();
+    let mut last_number = *last_number;
     let mut i = 0;
 
-    for n in INPUT {
-        if i < INPUT.len() - 1 {
-            last_index.insert(*n, i);
-        }
-
+    for n in predefined_numbers {
+        last_index.insert(*n, i);
         i += 1;
-        last_number = *n;
     }
 
-    while i < GOAL {
+    i += 1;
+
+    while i < goal {
         let next_number = if let Some(index) = last_index.get(&last_number) {
             i - 1 - index
         } else {
@@ -32,5 +33,5 @@ fn main() {
         last_number = next_number;
     }
 
-    println!("Part 1: {}", last_number);
+    last_number
 }
