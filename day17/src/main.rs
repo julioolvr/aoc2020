b3e99db84a924045fd4a53379253edf3a12e5e09
Julio <julio.olvr@gmail.com>
2020-12-18 09:18:10 -0300

    Day 17 Part 2

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
     reader.lines().enumerate().for_each(|(y, line)| {
         line.unwrap().chars().enumerate().for_each(|(x, c)| {
             if c == '#' {
-                initial_points.insert(Point {
+                initial_points.insert(Point2D {
                     x: x as isize,
                     y: y as isize,
                     z,
@@ -24,32 +24,49 @@ fn main() {
         });
     });
 
-    let mut dimension = Dimension::new(initial_points);
+    let mut dimension = Dimension::new(initial_points.clone());
 
     for _ in 0..6 {
         dimension.simulate();
     }
 
     let part_1 = dimension.active_points.len();
+    assert_eq!(part_1, 237);
     println!("Part 1: {}", part_1);
+
+    let initial_4d_points: HashSet<Point4D> =
+        initial_points.iter().map(|point| point.into()).collect();
+
+    let mut dimension = Dimension::new(initial_4d_points);
+
+    for _ in 0..6 {
+        dimension.simulate();
+    }
+
+    let part_2 = dimension.active_points.len();
+    println!("Part 2: {}", part_2);
+}
+
+trait Point: Sized + Clone + Eq + std::hash::Hash + Copy {
+    fn neighbors(&self) -> HashSet<Self>;
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
-struct Point {
+struct Point2D {
     x: isize,
     y: isize,
     z: isize,
 }
 
-impl Point {
-    fn neighbors(&self) -> HashSet<Point> {
+impl Point for Point2D {
+    fn neighbors(&self) -> HashSet<Point2D> {
         let mut neighbors = HashSet::new();
 
         for x in self.x - 1..=self.x + 1 {
             for y in self.y - 1..=self.y + 1 {
                 for z in self.z - 1..=self.z + 1 {
                     if x != self.x || y != self.y || z != self.z {
-                        neighbors.insert(Point { x, y, z });
+                        neighbors.insert(Point2D { x, y, z });
                     }
                 }
             }
@@ -59,25 +76,66 @@ impl Point {
     }
 }
 
-impl fmt::Display for Point {
+impl fmt::Display for Point2D {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "({},{},{})", self.x, self.y, self.z)
     }
 }
 
-struct Dimension {
-    active_points: HashSet<Point>,
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+struct Point4D {
+    x: isize,
+    y: isize,
+    z: isize,
+    w: isize,
 }
 
-impl Dimension {
-    fn new(active_points: HashSet<Point>) -> Dimension {
-        Dimension { active_points }
+impl Point for Point4D {
+    fn neighbors(&self) -> HashSet<Point4D> {
+        let mut neighbors = HashSet::new();
+
+        for x in self.x - 1..=self.x + 1 {
+            for y in self.y - 1..=self.y + 1 {
+                for z in self.z - 1..=self.z + 1 {
+                    for w in self.w - 1..=self.w + 1 {
+                        if x != self.x || y != self.y || z != self.z || w != self.w {
+                            neighbors.insert(Point4D { x, y, z, w });
+                        }
+                    }
+                }
+            }
+        }
+
+        neighbors
+    }
+}
+
+impl From<&Point2D> for Point4D {
+    fn from(point: &Point2D) -> Self {
+        Point4D {
+            x: point.x,
+            y: point.y,
+            z: point.z,
+            w: 0,
+        }
+    }
+}
+
+struct Dimension<T: Point> {
+    active_points: HashSet<T>,
+}
+
+impl<T: Point> Dimension<T> {
+    fn new(active_points: HashSet<T>) -> Dimension<T> {
+        Dimension {
+            active_points: active_points.clone(),
+        }
     }
 
     fn simulate(&mut self) {
         let current_points = self.active_points.clone();
 
-        let points_to_check: HashSet<Point> =
+        let points_to_check: HashSet<T> =
             current_points
                 .iter()
                 .fold(current_points.clone(), |mut acc, point| {
@@ -106,37 +164,37 @@ mod tests {
 
     #[test]
     fn test_point_neighbors() {
-        let point = Point { x: 2, y: 3, z: 4 };
+        let point = Point2D { x: 2, y: 3, z: 4 };
         let neighbors = point.neighbors();
         let mut expected = HashSet::new();
 
         expected.extend(vec![
-            Point { x: 1, y: 2, z: 3 },
-            Point { x: 2, y: 2, z: 3 },
-            Point { x: 3, y: 2, z: 3 },
-            Point { x: 1, y: 3, z: 3 },
-            Point { x: 2, y: 3, z: 3 },
-            Point { x: 3, y: 3, z: 3 },
-            Point { x: 1, y: 4, z: 3 },
-            Point { x: 2, y: 4, z: 3 },
-            Point { x: 3, y: 4, z: 3 },
-            Point { x: 1, y: 2, z: 4 },
-            Point { x: 2, y: 2, z: 4 },
-            Point { x: 3, y: 2, z: 4 },
-            Point { x: 1, y: 3, z: 4 },
-            Point { x: 3, y: 3, z: 4 },
-            Point { x: 1, y: 4, z: 4 },
-            Point { x: 2, y: 4, z: 4 },
-            Point { x: 3, y: 4, z: 4 },
-            Point { x: 1, y: 2, z: 5 },
-            Point { x: 2, y: 2, z: 5 },
-            Point { x: 3, y: 2, z: 5 },
-            Point { x: 1, y: 3, z: 5 },
-            Point { x: 2, y: 3, z: 5 },
-            Point { x: 3, y: 3, z: 5 },
-            Point { x: 1, y: 4, z: 5 },
-            Point { x: 2, y: 4, z: 5 },
-            Point { x: 3, y: 4, z: 5 },
+            Point2D { x: 1, y: 2, z: 3 },
+            Point2D { x: 2, y: 2, z: 3 },
+            Point2D { x: 3, y: 2, z: 3 },
+            Point2D { x: 1, y: 3, z: 3 },
+            Point2D { x: 2, y: 3, z: 3 },
+            Point2D { x: 3, y: 3, z: 3 },
+            Point2D { x: 1, y: 4, z: 3 },
+            Point2D { x: 2, y: 4, z: 3 },
+            Point2D { x: 3, y: 4, z: 3 },
+            Point2D { x: 1, y: 2, z: 4 },
+            Point2D { x: 2, y: 2, z: 4 },
+            Point2D { x: 3, y: 2, z: 4 },
+            Point2D { x: 1, y: 3, z: 4 },
+            Point2D { x: 3, y: 3, z: 4 },
+            Point2D { x: 1, y: 4, z: 4 },
+            Point2D { x: 2, y: 4, z: 4 },
+            Point2D { x: 3, y: 4, z: 4 },
+            Point2D { x: 1, y: 2, z: 5 },
+            Point2D { x: 2, y: 2, z: 5 },
+            Point2D { x: 3, y: 2, z: 5 },
+            Point2D { x: 1, y: 3, z: 5 },
+            Point2D { x: 2, y: 3, z: 5 },
+            Point2D { x: 3, y: 3, z: 5 },
+            Point2D { x: 1, y: 4, z: 5 },
+            Point2D { x: 2, y: 4, z: 5 },
+            Point2D { x: 3, y: 4, z: 5 },
         ]);
 
         assert_eq!(neighbors, expected);
